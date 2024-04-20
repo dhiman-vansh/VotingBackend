71a1091da6a91f4128682ed25eb1154b02b79acf
swish <vansh.2210@outlook.com>
2024-04-20 08:08:32 +0530

    Put added

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const Candidate = require("./models/Candidate");
 const app = express();
+const namelist = require("./models/namelist.json");
+const namesArray = namelist.map(item => item.name)
+console.log(namesArray)
+
+
+function checkIfNameExists(nameToCheck, namesArray) {
+  return namesArray.namelist(item => item.name === nameToCheck);
+}
+
+
 app.use(cors());
 app.use(express.json());
 
@@ -31,6 +41,30 @@ app.post("/", async(req, res) => {
     }
 });
 
+app.put("/:sr", async(req, res) => {
+
+  const srNo = req.params.sr;
+  const name = req.body.name;
+  // const nameExists = checkIfNameExists(name, namesArray);
+  if(namesArray.includes(name)){
+    res.send("PUT hit "+srNo+" "+name);
+    
+    try{
+      const candidate = await Candidate.findOne({srNo:srNo});
+      candidate.votes = candidate.votes + 1;
+      await candidate.save();
+      console.log(candidate);
+    }
+    catch(err){
+      res.status(400).json("error in sending data");
+    }
+
+  }
+  else {
+    res.send("NameError");
+  }
+
+});
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
